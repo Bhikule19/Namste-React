e9b35bb0b792acd7f6a8370047506a33ca55e63f
Abhishek <abhishekbhikule76@gmail.com>
2023-12-19 19:05:27 +0530

    Remove large file from version control

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
-/* <div id="parent">
-    <div id="child" >
-        <h1>" I am an H1 tag"</h1>
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// App Layout
+// Header
+// - logo
+// - nav items
+// Body
+// - Search
+// - Restuarant container
+//   -Restuarant card
+// Image, price, est, ratings
+// Footer
+// -Copyright
+// -link
+// -add
+// -contact
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
+  );
+};
 
-</div> */
+const RestuarantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://thumbs.dreamstime.com/b/pizza-rustic-italian-mozzarella-cheese-basil-leaves-35669930.jpg"
+      ></img>
+      <h4>LaPino's Pizza</h4>
+      <h4>Pizza, Indian, North Indian</h4>
+      <h4>4.4 star</h4>
+      <h4>30 minutes</h4>
+    </div>
+  );
+};
 
-// Lets build the above html code into react ( Nested Structure in react)
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+      </div>
+    </div>
+  );
+};
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an H1 tag"),
-    React.createElement("h2", {}, "I am an H2 tag"),
-  ]) //to add more than one children you can do it by using array
-);
+const Applayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<Applayout />);
